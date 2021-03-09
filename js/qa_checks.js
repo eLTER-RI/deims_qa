93d263f7a79969f7134abe09ff9b00a0be358e56
stopopol <christoph.wohner@umweltbundesamt.at>
2021-03-09 15:40:34 +0100

    Update qa_checks.js

--- js/qa_checks.js
+++ js/qa_checks.js
@@ -11,7 +11,6 @@ function calculate_completeness(jsonObj) {
 	site_attributes['general']['siteType'] ? number_of_filled_fields++ : list_of_missing_fields += '<li>Site Type</li>';
 	site_attributes['contact']['siteManager'] ? number_of_filled_fields++ : list_of_missing_fields += '<li>Site Manager</li>';
 	site_attributes['contact']['metadataProvider'] ? number_of_filled_fields++ : list_of_missing_fields += '<li>Metadata provider</li>';
-	site_attributes['affiliation']['networks'] ? number_of_filled_fields++ : list_of_missing_fields += '<li>Networks</li>';
 	site_attributes['geographic']['coordinates'] ? number_of_filled_fields++ : list_of_missing_fields += '<li>Coordinates</li>';
 	site_attributes['geographic']['country'] ? number_of_filled_fields++ : list_of_missing_fields += '<li>Country</li>';
 	site_attributes['geographic']['elevation']['min'] != null ? number_of_filled_fields++ : list_of_missing_fields += '<li>Elevation (min)</li>';
@@ -31,7 +30,7 @@ function calculate_completeness(jsonObj) {
 	site_attributes['infrastructure']['operation']['permanent'] != null ? number_of_filled_fields++ : list_of_missing_fields += '<li>Permanent Operation</li>';
 
 	list_of_missing_fields += '</ul>'
-	var percentage = (number_of_filled_fields/25*100).toFixed(2) + '%';
+	var percentage = (number_of_filled_fields/24*100).toFixed(2) + '%';
 	
 	if (percentage == '100.00%') {
 		document.getElementById('completeness_check_result').innerHTML = '<div class="text-success">' + percentage + '</div>';
